fa383dbcbd8a6acd6fb3eefe1646538ce35c00bd
Yash Agarwal <yashagarwaljpr@gmail.com>
2018-03-04 02:44:03 +0530

    add sqlite support

--- recognizer.py
+++ recognizer.py
@@ -1,20 +1,53 @@
 import face_recognition
 import cv2
 import os
+import sqlite3
+
 
 class recognizer:
+
     def __init__(self):
         self.encodings = []
         self.videoCapture = None
         self.names = []
+        self.conn = None
+        self.filePath = os.path.realpath(
+            os.path.join(os.getcwd(), os.path.dirname(__file__)))
+
+    def createConnection(self, db_file):
+        try:
+            self.conn = sqlite3.connect(db_file)
+        except Error as e:
+            print(e)
+
+    def insertInDB(self, name, imagePath, cursor):
+        query = cursor.execute(
+            'SELECT Name from faces where Name = ?', (name, ))
+        if not query.fetchall():
+            cursor.execute(
+                'INSERT INTO faces (Name, ImagePath) VALUES (?, ?)', (name, imagePath,))
 
     def loadImages(self):
-        for imageFile in os.listdir('images'):
-            image = face_recognition.load_image_file(os.path.join('images',imageFile))
+        db_file = os.path.join(self.filePath, 'data.db')
+        self.createConnection(db_file)
+        db = self.conn.cursor()
+        db.execute("CREATE TABLE IF NOT EXISTS faces(`Name`  TEXT, \
+                                                     `id`    INTEGER DEFAULT 0 PRIMARY KEY AUTOINCREMENT, \
+                                                     `ImagePath` TEXT)")
+        imageDirectory = os.path.join(self.filePath, 'images')
+        db.execute('SELECT * FROM faces')
+        for person in db.fetchall():
+            image = face_recognition.load_image_file(
+                os.path.join(imageDirectory, person[2]))
             encoding = face_recognition.face_encodings(image)[0]
             self.encodings.append(encoding)
-            name = imageFile.split('.')[0].replace('_',' ')
-            self.names.append(name)
+            # name = imageFile.split('.')[0].replace('_', ' ')
+            self.names.append(person[0])
+            # self.insertInDB(name, imageFile, db)
+
+        print self.names
+        self.conn.commit()
+        self.conn.close()
 
     def startVideoCapture(self, devNum):
         try:
@@ -33,14 +66,16 @@ class recognizer:
             if frame is None:
                 print "Failed to get frames!"
                 exit()
-            
+
             rgbFrame = frame[:, :, ::-1]
 
             face_locations = face_recognition.face_locations(rgbFrame)
-            face_encodings = face_recognition.face_encodings(rgbFrame, face_locations)
+            face_encodings = face_recognition.face_encodings(
+                rgbFrame, face_locations)
 
             for (top, right, bottom, left), face_encoding in zip(face_locations, face_encodings):
-                matches = face_recognition.compare_faces(self.encodings, face_encoding)
+                matches = face_recognition.compare_faces(
+                    self.encodings, face_encoding)
 
                 name = "Unknown"
 
@@ -48,11 +83,14 @@ class recognizer:
                     first_match_index = matches.index(True)
                     name = self.names[first_match_index]
 
-                cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
+                cv2.rectangle(frame, (left, top),
+                              (right, bottom), (0, 0, 255), 2)
 
-                cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), -1)
+                cv2.rectangle(frame, (left, bottom - 35),
+                              (right, bottom), (0, 0, 255), -1)
                 font = cv2.FONT_HERSHEY_DUPLEX
-                cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
+                cv2.putText(frame, name, (left + 6, bottom - 6),
+                            font, 1.0, (255, 255, 255), 1)
 
             cv2.imshow('Video', frame)
 
@@ -61,3 +99,11 @@ class recognizer:
 
         self.videoCapture.release()
         cv2.destroyAllWindows()
+
+
+def main():
+    obj = recognizer()
+    obj.start(0)
+
+if __name__ == "__main__":
+    main()
